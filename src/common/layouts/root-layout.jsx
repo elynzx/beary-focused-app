67466820c5d64e792c5928e68914a86d9cdaf9b1
Elynzx <rpascual06@outlook.com>
2026-04-19 11:07:32 -0500

    Fix formatting in Sidebar component and update base path configuration in vite.config.js

--- src/common/layouts/root-layout.jsx
+++ src/common/layouts/root-layout.jsx
@@ -11,7 +11,7 @@ export function RootLayout() {
         <div className="h-screen w-full bg-bgLightPink text-bgGray flex items-center justify-center p-4 md:p-12">
             <div className="flex h-full w-full max-w-7xl flex-col bg-bgPink/20 rounded-4xl overflow-hidden border border-bgDarkGray/50 shadow-lg">
                 <div className="flex flex-col lg:flex-row flex-1 overflow-hidden">
-                    <Sidebar/>
+                    <Sidebar />
                     <main className="flex-1 flex flex-col overflow-hidden">
                         <Outlet
                             context={{
