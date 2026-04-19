9e5b3ac310d74f207c9199202253c56d5db82a8c
Elynzx <rpascual06@outlook.com>
2026-04-18 19:08:06 -0500

    Add Sidebar component for navigation and timer display

---
+++ src/common/components/sidebar/Sidebar.jsx
@@ -0,0 +1,35 @@
+import { Timer } from "../timer/Timer";
+import { Link } from "react-router";
+
+export function Sidebar() {
+    return (
+        <aside className="w-68 border-r items-center border-black hidden lg:flex flex-col p-6">
+            <Timer />
+            <nav className="mt-8 space-y-2 flex lg:flex-col">
+                <Link
+                    className="rounded-full flex items-center justify-center w-50 h-10 bg-bgGray text-white hover:bg-bgDarkGray"
+                    to="/"
+                >
+                    Home
+                </Link>
+                <Link
+                    className="rounded-full flex items-center justify-center w-50 h-10 bg-bgGray text-white hover:bg-bgDarkGray"
+                    to="stats"
+                >
+                    Stats
+                </Link>
+                <Link
+                    className="rounded-full flex items-center justify-center w-50 h-10 bg-bgGray text-white hover:bg-bgDarkGray"
+                    to="settings"
+                >
+                    Settings
+                </Link>
+            </nav>
+            <div className="mt-auto w-full text-center p-4 border-t border-bgDarkGray">
+                <p className="text-[12px] text-bgDarkGray italic">
+                    built with love ꒰ ˶• ༝ •˶꒱
+                </p>
+            </div>
+        </aside>
+    );
+}
