23e501edb91de3efd9d747a98daed261648614a7
Elynzx <rpascual06@outlook.com>
2026-04-18 19:07:57 -0500

    Refactor RootLayout component structure and styling for improved layout and responsiveness

--- src/common/layouts/root-layout.jsx
+++ src/common/layouts/root-layout.jsx
@@ -1,10 +1,21 @@
 import { Outlet } from "react-router";
+import { Sidebar } from "../components/sidebar/Sidebar";
+import { Header } from "../components/header/header";
 
 export function RootLayout() {
     return (
-        <div className="min-h-screen bg-bgLightPink text-bgGray">
-            <div className="flex min-h-screen flex-col lg:flex-row">
-                <Outlet />
+        <div className="h-screen w-full bg-white text-bgGray flex items-center justify-center py-12">
+            <div className="flex h-full w-full max-w-7xl flex-col bg-bgLightPink rounded-4xl overflow-hidden border border-black shadow-lg">
+
+                <div className="flex flex-1 overflow-hidden">
+                    <Sidebar />
+
+                    <main className="w-full h-full flex-1">
+                        <section>
+                            <Outlet />
+                        </section>
+                    </main>
+                </div>
             </div>
         </div>
     );
