1aadfb469db1f8d316eed3a1bb87bbe49487d6d2
Elynzx <rpascual06@outlook.com>
2026-04-18 19:07:40 -0500

    Add Board component as a child route under RootLayout

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,9 +1,16 @@
 import { createBrowserRouter } from "react-router";
 import { RootLayout } from "../common/layouts/root-layout";
+import { Board } from "../app/board/board";
 
 export const router = createBrowserRouter([
     {
         path: "/",
         Component: RootLayout,
+        children: [
+            {
+                index: true,
+                Component: Board,
+            },
+        ],
     },
 ]);
