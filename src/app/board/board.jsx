5a6486252c7b1e832277d017f89168a4fc1a1958
Elynzx <rpascual06@outlook.com>
2026-04-18 19:08:41 -0500

    Add Board and BoardPage components for task management interface

---
+++ src/app/board/board.jsx
@@ -0,0 +1,5 @@
+import { BoardPage } from "../../features/board/pages/board/boardPage";
+
+export function Board() {
+    return <BoardPage />;
+}
---
+++ src/features/board/pages/board/boardPage.jsx
@@ -0,0 +1,29 @@
+import { TaskCard } from "../../components/taskCard/taskCard";
+import { QUADRANTS } from "../../utils/constants";
+import { useTasks } from "../../hooks/useTasks";
+import { Header } from "../../../../common/components/header/header";
+
+export function BoardPage() {
+    const { tasks, addTask, deleteTask, updateStatus, updateTask } = useTasks();
+
+    return (
+        <div className="flex flex-col w-full h-full">
+            <Header onAdd={addTask} />
+            <div className="grid grid-cols-2 grid-rows-2 gap-6 p-6 flex-1 overflow-auto">
+                {QUADRANTS.map((quadrant) => (
+                    <TaskCard
+                        key={quadrant.id}
+                        title={quadrant.label}
+                        subtitle={quadrant.description}
+                        tasks={tasks.filter(
+                            (task) => task.quadrant === quadrant.id,
+                        )}
+                        onDelete={deleteTask}
+                        onUpdateStatus={updateStatus}
+                        onUpdatetask={updateTask}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+}
